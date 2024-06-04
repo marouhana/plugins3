cbc6b6626ee7756245024ea7155009bd111f5eb5
hanae <maroufhanae@gmail.com>
2024-06-04 10:31:27 +0200

    pluging

--- cypress.config.js
+++ cypress.config.js
@@ -6,14 +6,16 @@
       /// implement node event listeners here
   // },
     const { defineConfig } = require('cypress')
-
+     
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // bind les événements qu'on veut
-      on('<event>', (arg1, arg2) => {
+      //on('<event>', (arg1, arg2) => {
         // plugin stuff here
-      })
+      //)
     },
   },
 
